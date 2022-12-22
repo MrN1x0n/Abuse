fd5c829adc973c662c4519e278c89bce0b1aebe1
Nixon Ilya <95374645+MrN1x0n@users.noreply.github.com>
2022-12-22 18:36:54 +0300

    Create abuse.sh

---
+++ abuse.sh
@@ -0,0 +1,14 @@
+file=$(cat proxy.txt)
+
+for line in $file
+do
+i=21
+nibid keys add wallet$i --keyring-backend test
+wallet_now=$(nibid keys show wallet$i --keyring-backend test -a)
+
+curl --proxy $line -X POST -d '{"address": "$wallet_now", "coins": ["100000000000unibi","100000000000unusd"]}' "https://faucet.testnet-2.nibiru.fi/"
+echo $wallet_now >> $HOME/wallets.txt
+sleep 1
+nibid tx bank send $wallet_now $my_wallet 109990000unibi --keyring-backend test  --chain-id $NIBIRU_CHAIN_ID --fees 6000unibi -y
+i=i+1
+done
