e32c8082ebccd5b7bda01f810506c45bbd9a5448
Nixon Ilya <95374645+MrN1x0n@users.noreply.github.com>
2022-12-22 20:37:19 +0300

    Update abuse.sh

--- abuse.sh
+++ abuse.sh
@@ -2,13 +2,13 @@ file=$(cat proxy.txt)
 i=30
 for line in $file
 do
-nibid keys add wallet$i --keyring-backend test
-wallet_now=$(nibid keys show wallet$i --keyring-backend test -a)
+nibid keys add wallet$i
+wallet_now=$(nibid keys show wallet$i -a)
 sleep 1
 echo "Запрашиваем токены"
 curl --proxy $line -X POST -d '{"address": "'"$wallet_now"'", "coins": ["110000000unibi","100000000000unusd"]}' "https://faucet.testnet-2.nibiru.fi/"
 echo $wallet_now >> $HOME/wallets.txt
 sleep 1
-nibid tx bank send $wallet_now $my_wallet 109990000unibi --keyring-backend test  --chain-id $NIBIRU_CHAIN_ID --fees 6000unibi -y
+nibid tx bank send $wallet_now $my_wallet 109990000unibi --chain-id $NIBIRU_CHAIN_ID --fees 6000unibi -y
 let "i=i+1"
 done
