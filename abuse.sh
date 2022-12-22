7714d6ded6411b10d7421983bc2fec748b8c46d4
Nixon Ilya <95374645+MrN1x0n@users.noreply.github.com>
2022-12-22 19:25:47 +0300

    Update abuse.sh

--- abuse.sh
+++ abuse.sh
@@ -5,7 +5,7 @@ do
 nibid keys add wallet$i --keyring-backend test
 wallet_now=$(nibid keys show wallet$i --keyring-backend test -a)
 
-curl --proxy $line -X POST -d '{"address": "$wallet_now", "coins": ["100000000000unibi","100000000000unusd"]}' "https://faucet.testnet-2.nibiru.fi/"
+curl --proxy $line -X POST -d '{"address": "'"$wallet_now"'", "coins": ["110000000unibi","100000000000unusd"]}' "https://faucet.testnet-2.nibiru.fi/"
 echo $wallet_now >> $HOME/wallets.txt
 sleep 1
 nibid tx bank send $wallet_now $my_wallet 109990000unibi --keyring-backend test  --chain-id $NIBIRU_CHAIN_ID --fees 6000unibi -y
